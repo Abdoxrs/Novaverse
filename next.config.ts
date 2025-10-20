54f2af7b3512e1d0f2fc508376851a86dd8b3bb1
AbdoX <abdoxrs223@gmail.com>
2025-10-21 02:57:54 +0300

    Test2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable static HTML export (replaces deprecated `next export` behavior)
+  // See: https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+  output: 'export',
 };
 
 export default nextConfig;
